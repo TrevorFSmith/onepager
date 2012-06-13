7a9f6adda90720c6546b6a256fd2b3efd121d263
Trevor F. Smith <trevor@trevor.smith.name>
2012-06-13 09:14:51 -0700

    Now showing seattle signage on google maps using fusiontables.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var onepage = onepage || {};
 onepage.views = onepage.views || {};
 
+/*
+    <script type="text/javascript">
+      function initialize() {
+        var myOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            myOptions);
+      }
+    </script>
+  </head>
+  <body onload="initialize()">
+    <div id="map_canvas" style="width:100%; height:100%"></div>
+  </body>
+*/
 onepage.Router = Backbone.Router.extend({
 	routes: {
 		"":"home",
@@ -47,7 +64,20 @@ onepage.views.HomeView = Backbone.View.extend({
 		_.bindAll(this, 'render');
 	},
 	render: function(){
-		this.$el.append('<h1>Home:</h1>');
+		var mapOptions = {
+			center: new google.maps.LatLng(47.66620925,-122.31716518),
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(this.el, mapOptions);
+
+		var signLayer = new google.maps.FusionTablesLayer({
+			query: {
+				select: 'Latitude',
+				from: '1DMT1bhBsnSqyJDPizMM9BZ2tgkDWXHHunfkUi00'
+			},
+		});
+		signLayer.setMap(map);
 		return this;
 	},
 });
@@ -57,15 +87,24 @@ onepage.views.AboutView = Backbone.View.extend({
 	id: 'aboutView',
 	initialize: function(){
 		_.bindAll(this, 'render');
+		this.template = $("#aboutTemplate").html();
 	},
 	render: function(){
-		this.$el.append('<h1>About:</h1>');
+		this.$el.html(_.template(this.template, {}));
 		return this;
 	},
 });
 
 $(document).ready(function(){
-	window.pageView = new onepage.views.PageView({el:"#pageView"});
-	window.pageView.render();
-	Backbone.history.start();
+	var mapScriptURL = 'http://maps.googleapis.com/maps/api/js?key=' + settings.googleAPIKey + '&sensor=false&callback=gmap_draw';
+	var s = document.createElement("script");
+	s.type = "text/javascript";
+	s.src  = mapScriptURL;
+
+	window.gmap_draw = function(){
+		window.pageView = new onepage.views.PageView({el:"#pageView"});
+		window.pageView.render();
+		Backbone.history.start();
+	};
+	$("head").append(s);  
 });

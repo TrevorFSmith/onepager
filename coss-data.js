d524cb7df03c4ad16bd7118ff42f0b44d94f0105
Trevor F. Smith <trevor@trevor.smith.name>
2012-12-28 10:59:50 -0800

    Now sorting by date and displaying the next upcoming day

--- coss-data.js
+++ coss-data.js
@@ -1,16 +1,30 @@
 [
 	{
-		"date": "3", "month": "2",
+		"date": 28, "month": 3,
+		"name": "Biffon Morris Guitiarre",
+		"themes": ["attraction", "passion"],
+		"patronage": ["lovers", "enemies", "salespeople"],
+		"body": "This is a test of multi-line text. \n Does this work?"
+	},
+	{
+		"date": 3, "month": 2,
 		"name": "Albert Examplestein",
 		"themes": ["attraction", "passion"],
 		"patronage": ["lovers", "enemies", "salespeople"],
 		"body": "This is a test of multi-line text. \n Does this work?  This is a fair amount of text so that we can see how it wraps and how the spacing works."
 	},
 	{
-		"date": "28", "month": "3",
-		"name": "Biffon Morris Guitiarre",
-		"themes": ["attraction", "passion"],
-		"patronage": ["lovers", "enemies", "salespeople"],
+		"date": 27, "month": 12,
+		"name": "Mooftie Blowhard McGuffin",
+		"themes": ["poofing", "wind"],
+		"patronage": ["ginches", "flower salesmen", "the dead"],
+		"body": "This is a test of multi-line text. \n Does this work?"
+	},
+	{
+		"date": 31, "month": 12,
+		"name": "Boog Hflaks Mkkiis",
+		"themes": ["poofing", "wind"],
+		"patronage": ["ginches", "flower salesmen", "the dead"],
 		"body": "This is a test of multi-line text. \n Does this work?"
 	}
 ]
--- coss.js
+++ coss.js
@@ -2,12 +2,28 @@ var coss = coss || {};
 
 coss.api = coss.api || {};
 
-coss.api.Saint = Backbone.Model.extend({});
+coss.monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+coss.api.Saint = Backbone.Model.extend({
+	initialize: function(){
+		_.bindAll(this);
+	},
+	getDate: function(){
+		return coss.constructDate(this.get('month'), this.get('date'));
+	},
+	getDateDisplay: function(){
+		return coss.monthNames[this.get('month') - 1] + ' ' + this.get('date');
+	},
+});
 
 coss.api.Saints = Backbone.Collection.extend({
 	model:coss.api.Saint,
 	url:'coss-data.js',
+
+	// By default, sort the Saints by date
+	comparator: function(saint) { return saint.getDate().getTime(); },
 });
+
 
 coss.views = coss.views || {};
 
@@ -20,7 +36,27 @@ coss.views.DaysFlipView = Backbone.View.extend({
 	initialize: function(){
 		_.bindAll(this);
 		this.saintIndex = 0;
-		this.options.saints.on('reset', this.render);
+		this.options.saints.on('reset', this.handleReset);
+	},
+	handleReset: function(){
+		// find the next saint
+		this.options.saints.sort();
+		this.saintIndex = 0;
+		var today = coss.constructDate(new Date().getMonth() + 1, new Date().getDate());
+		for(var i =0; i < this.options.saints.length; i++){
+			var saintDate = this.options.saints.at(i).getDate();
+			if(saintDate.getTime() == today.getTime()){	// if today is a saint's day, use that
+				this.saintIndex = i;
+				break;
+			} else if(saintDate < today){ // if this day is in the past and the next day is in the future, use that
+				if(i < this.options.saints.length - 1 && this.options.saints.at(i + 1).getDate() > today){
+					this.saintIndex = i + 1;
+					break;
+				}
+			}
+			// else, default to the first in the list
+		}
+		this.render();
 	},
 	flipLeft: function(){ return this.flip(-1); },
 	flipRight: function(){ return this.flip(1); },
@@ -46,7 +82,7 @@ coss.views.DaysFlipView = Backbone.View.extend({
 		this.$el.append(rightAnchor);
 
 		var saint = this.options.saints.at(this.saintIndex);
-		this.$el.append($.el.h2(coss.formatDate(2012, saint.get('month'), saint.get('date'))));
+		this.$el.append($.el.h2(saint.getDateDisplay()));
 		var dayDetailView = new coss.views.DayDetailView({model:saint});
 		this.$el.append(dayDetailView.render().el);
 		return this;
@@ -84,13 +120,17 @@ coss.views.DayDetailView = Backbone.View.extend({
 	},
 })
 
-coss.formatDate = function(year, month, day){
+coss.constructDate = function(month, day){
 	var d = new Date();
-	d.setUTCFullYear(parseInt(year));
-	d.setUTCMonth(parseInt(month) - 1);
-	d.setUTCDate(parseInt(day));
-	d.setUTCHours(12);
-	d.setUTCMinutes(0);
-	d.setUTCSeconds(0);
-	return d.toLocaleDateString();  
+	d.setMonth(parseInt(month) - 1);
+	d.setDate(parseInt(day));
+	d.setHours(12);
+	d.setMinutes(0);
+	d.setSeconds(0);
+	d.setMilliseconds(0)
+	return d;	
+}
+
+coss.formatDate = function(month, day){
+	return coss.constructDate(month, day).toLocaleDateString();
 }

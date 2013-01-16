990e964e0bfb390a34290ae7fada856baefe0df7
Trevor F. Smith <trevor@trevor.smith.name>
2013-01-15 17:30:24 -0800

    Added new jQuery UI.  Started drag to create and draggable BoxViews.

--- index.js
+++ index.js
@@ -40,14 +40,86 @@ onepage.views.PageView = Backbone.View.extend({
 	},
 });
 
+onepage.views.BoxView = Backbone.View.extend({
+	className: 'boxView',
+	initialize: function(){
+		_.bindAll(this);
+		this.$el.draggable({
+			cursor:'move',
+			create:this.dragCreate,
+			start:this.dragStart,
+			drag:this.dragDrag,
+			stop:this.dragStop,
+		});
+	},
+	render: function(){
+		this.$el.empty();
+		return this;
+	},
+	dragCreate: function(event){
+	},
+	dragStart: function(event){
+	},
+	dragDrag: function(event){
+	},
+	dragStop: function(event){
+	},
+})
+
+onepage.views.DragCreateHelper = Backbone.View.extend({
+	className:'dragCreateHelper',
+	initialize: function(){
+		_.bindAll(this);
+	},
+	render: function(){
+		this.$el.empty();
+		return this;
+	},
+})
+
 onepage.views.HomeView = Backbone.View.extend({
 	className: 'routeView',
 	id: 'homeView',
 	initialize: function(){
-		_.bindAll(this, 'render');
+		_.bindAll(this);
+		this.startPosition = null;
+		this.draggingBoxView = null;
+		this.helperView = new onepage.views.DragCreateHelper();
+
+		this.$el.draggable({
+			cursor:'move',
+			cursorAt: { top: 0, left: 0 },
+			helper: _.bind(function(event){
+				// Return an empty helper so that we don't move this view but render no helper
+				return this.helperView.el;
+			}, this),
+			start:this.dragStart,
+			drag:this.dragDrag,
+			stop:this.dragStop,
+		});
+	},
+	dragStart: function(event){
+		this.startPosition = [event.offsetX, event.offsetY];
+		this.draggingBoxView = new onepage.views.BoxView();
+		this.draggingBoxView.$el.css({position: 'absolute', top:event.offsetY + 'px', left:event.offsetX + 'px'});
+		this.$el.append(this.draggingBoxView.render().el);
+	},
+	dragDrag: function(event){
+		var mousePosition = this.helperView.$el.position();
+		var viewPosition = this.$el.position();
+		var mouseOffset = [mousePosition.left - viewPosition.left, mousePosition.top - viewPosition.top];
+		var width = Math.abs(this.startPosition[0] - mouseOffset[0]);
+		var height = Math.abs(this.startPosition[1] - mouseOffset[1]);
+		var left = this.startPosition[0];
+		if(this.startPosition[0] - mouseOffset[0] > 0) left = this.startPosition[0] - width;
+		var top = this.startPosition[1];
+		if(this.startPosition[1] - mouseOffset[1] > 0) top = this.startPosition[1] - height;
+		this.draggingBoxView.$el.css({'top':top, 'left':left, 'width':width, 'height':height});
+	},
+	dragStop: function(event){
 	},
 	render: function(){
-		this.$el.append($.el.h1('Home:'));
+		this.$el.empty()
 		return this;
 	},
 });
